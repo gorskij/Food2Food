ceeaa84bb565ff64e7f565ac17d73d40cd28e920
Jakub Górski <jakub1q23@gmail.com>
2024-11-28 01:45:25 +0100

    feat: added ProductAllergens to ProductDetailsPage

---
+++ webapp/src/components/ProductAllergens.tsx
@@ -0,0 +1,42 @@
+import { ProductDetails } from "@/types/ProductDetails";
+import { FC } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+
+interface ProductAllergensProps {
+  productDetails: ProductDetails;
+}
+
+const ProductAllergens: FC<ProductAllergensProps> = ({ productDetails }) => {
+  const allergens = productDetails.label.allergens;
+  const areAllergensAbsent = allergens.length === 0;
+
+  return (
+    <Card
+      className={`flex-1 max-w-full mt-4 ${
+        areAllergensAbsent ? "bg-green-100" : ""
+      }`}
+    >
+      <CardHeader>
+        <CardTitle className="text-left">Alergeny</CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-wrap gap-2">
+        {allergens.length > 0 ? (
+          allergens.map((allergen) => (
+            <div
+              key={allergen.id}
+              className="px-4 py-2 bg-red-100 text-red-600 rounded-full border border-red-300 text-sm font-medium"
+            >
+              {allergen.name}
+            </div>
+          ))
+        ) : (
+          <p className="flex-1 text-center text-xl text-muted-foreground">
+            Brak alergenów
+          </p>
+        )}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ProductAllergens;
