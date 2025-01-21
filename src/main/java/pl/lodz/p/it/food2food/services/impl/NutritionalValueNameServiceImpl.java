2dc1426b0d068a4d5b674a2f2fae6e38f9659305
Jakub Górski <jakub1q23@gmail.com>
2025-01-21 14:22:59 +0100

    fix: transaction MANDATORY in services

--- src/main/java/pl/lodz/p/it/food2food/services/impl/NutritionalValueNameServiceImpl.java
+++ src/main/java/pl/lodz/p/it/food2food/services/impl/NutritionalValueNameServiceImpl.java
@@ -2,6 +2,8 @@ package pl.lodz.p.it.food2food.services.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import pl.lodz.p.it.food2food.model.NutritionalValueName;
 import pl.lodz.p.it.food2food.repositories.NutritionalValueNameRepository;
 import pl.lodz.p.it.food2food.services.NutritionalValueNameService;
@@ -9,6 +11,7 @@ import pl.lodz.p.it.food2food.services.NutritionalValueNameService;
 import java.util.List;
 
 @Service
+@Transactional(propagation = Propagation.REQUIRES_NEW)
 @RequiredArgsConstructor
 public class NutritionalValueNameServiceImpl implements NutritionalValueNameService {
     private final NutritionalValueNameRepository nutritionalValueNameRepository;
--- src/main/java/pl/lodz/p/it/food2food/services/impl/RatingServiceImpl.java
+++ src/main/java/pl/lodz/p/it/food2food/services/impl/RatingServiceImpl.java
@@ -2,6 +2,8 @@ package pl.lodz.p.it.food2food.services.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import pl.lodz.p.it.food2food.model.Rating;
 import pl.lodz.p.it.food2food.repositories.RatingRepository;
 import pl.lodz.p.it.food2food.services.RatingService;
@@ -9,6 +11,7 @@ import pl.lodz.p.it.food2food.services.RatingService;
 import java.util.List;
 
 @Service
+@Transactional(propagation = Propagation.REQUIRES_NEW)
 @RequiredArgsConstructor
 public class RatingServiceImpl implements RatingService {
     private final RatingRepository ratingRepository;
