e89ad55a1afc507ae5b4a779c970902d5c4ee256
Jakub Górski <jakub1q23@gmail.com>
2025-02-02 18:00:15 +0100

    feat: jwt secret to app properties

--- src/main/java/pl/lodz/p/it/food2food/services/impl/JwtService.java
+++ src/main/java/pl/lodz/p/it/food2food/services/impl/JwtService.java
@@ -4,8 +4,8 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -18,24 +18,22 @@ import pl.lodz.p.it.food2food.exceptions.NotFoundException;
 import pl.lodz.p.it.food2food.exceptions.handlers.ErrorCodes;
 import pl.lodz.p.it.food2food.exceptions.messages.UserExceptionMessages;
 import pl.lodz.p.it.food2food.model.User;
-import pl.lodz.p.it.food2food.repositories.AdministratorAccessLevelRepository;
 import pl.lodz.p.it.food2food.repositories.UserRepository;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @Transactional(propagation = Propagation.MANDATORY)
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class JwtService {
-    private final String secret_key = "very_secret_key_123!";
+    @Value("${jwt.secret}")
+    private String secretKey;
+
     private final UserRepository userRepository;
-    private final AdministratorAccessLevelRepository administratorAccessLevelRepository;
 
     @PreAuthorize("permitAll()")
     public String createToken(User user, List<String> roles) {
@@ -49,12 +47,12 @@ public class JwtService {
                 .withClaim("authorities", roles)
                 .withIssuedAt(Date.from(now.atZone(ZoneId.systemDefault()).toInstant()))
                 .withExpiresAt(Date.from(validity.atZone(ZoneId.systemDefault()).toInstant()))
-                .sign(Algorithm.HMAC256(secret_key));
+                .sign(Algorithm.HMAC256(secretKey));
     }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public Authentication validateToken(String token) throws NotFoundException {
-        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secret_key)).build();
+        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secretKey)).build();
         DecodedJWT decodedJWT = verifier.verify(token);
         User user = userRepository.findByUsername(decodedJWT.getSubject()).orElseThrow(() -> new NotFoundException(UserExceptionMessages.NOT_FOUND, ErrorCodes.USER_NOT_FOUND));
         List<GrantedAuthority> authorities = user.getAccessLevels().stream()
@@ -64,7 +62,7 @@ public class JwtService {
     }
 
     public String getUserId(String token) {
-        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secret_key)).build();
+        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secretKey)).build();
         DecodedJWT decodedJWT = verifier.verify(token);
         return decodedJWT.getClaim("id").asString();
     }
