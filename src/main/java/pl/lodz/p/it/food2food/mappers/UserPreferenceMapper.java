bde90457964cbe2698420d1d18722fbfe62bdbb1
Jakub Górski <jakub1q23@gmail.com>
2025-02-02 20:29:54 +0100

    ref: delete unused mappers

--- src/main/java/pl/lodz/p/it/food2food/mappers/UserPreferenceMapper.java
+++ src/main/java/pl/lodz/p/it/food2food/mappers/UserPreferenceMapper.java
@@ -4,35 +4,7 @@ import org.mapstruct.Mapper;
 import pl.lodz.p.it.food2food.dto.responses.UserPreferenceResponse;
 import pl.lodz.p.it.food2food.model.*;
 
-import java.util.Set;
-import java.util.UUID;
-import java.util.stream.Collectors;
-
 @Mapper(componentModel = "spring")
 public interface UserPreferenceMapper {
     UserPreferenceResponse toUserPreferenceResponse(UserPreference userPreference);
-
-    default Set<UUID> mapAllergens(Set<Allergen> allergens) {
-        return allergens.stream()
-                .map(Allergen::getId)
-                .collect(Collectors.toSet());
-    }
-
-    default Set<UUID> mapRatings(Set<Rating> ratings) {
-        return ratings.stream()
-                .map(Rating::getId)
-                .collect(Collectors.toSet());
-    }
-
-    default Set<UUID> mapNutritionalValueNames(Set<NutritionalValueName> nutritionalValueNames) {
-        return nutritionalValueNames.stream()
-                .map(NutritionalValueName::getId)
-                .collect(Collectors.toSet());
-    }
-
-    default Set<UUID> mapPackageTypes(Set<PackageType> packageTypes) {
-        return packageTypes.stream()
-                .map(PackageType::getId)
-                .collect(Collectors.toSet());
-    }
 }
